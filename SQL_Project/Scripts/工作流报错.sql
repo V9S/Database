e6fabb1b6a0cd12586fd96c9ce9dab3b53a4418d
V9S <2993953259@qq.com>
2020-10-26 10:53:05 +0800

    jbpm

--- SQL_Project/Scripts/工作流报错.sql
+++ SQL_Project/Scripts/工作流报错.sql
@@ -19,3 +19,23 @@ select * from nodeinstancelog n where n.externalId like '%3960bec0-c509-4abf-98e
 
 select * from processinstanceinfo p where p.InstanceId = '1037';
 
+
+
+-----------------------------------------------------------------------------
+select * from deploymentstore d where d.DEPLOYMENT_ID like '%077f9358-b3fb-4bea-a58d-6cdfb7648914%';
+
+select * from nodeinstancelog n where n.externalId like '%077f9358-b3fb-4bea-a58d-6cdfb7648914%';
+
+select * from nodeinstancelog n where n.externalId like '%077f9358-b3fb-4bea-a58d-6cdfb7648914%' and n.processInstanceId = 1044;
+
+select * from processinstanceinfo p where p.InstanceId = '1045';
+
+select * from processinstancelog p where p.processId = '1045';
+
+select * from processinstancelog p where p.externalId like '%077f9358-b3fb-4bea-a58d-6cdfb7648914%'
+
+select id,externalId,processid,processInstanceId,start_date from processinstancelog p where p.externalId like '%077f9358-b3fb-4bea-a58d-6cdfb7648914%'
+
+-- processinstanceinfo表的id保持和processinstancelog表的id相同
+
+alter table processinstanceinfo AUTO_INCREMENT=1060
