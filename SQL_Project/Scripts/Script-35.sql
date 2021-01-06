cbfada1e6435aae8b32b3e1da5ec801966a875b5
V9S <2993953259@qq.com>
2021-01-06 17:23:27 +0800

    sql

---
+++ SQL_Project/Scripts/Script-35.sql
@@ -0,0 +1,8 @@
+SELECT * FROM gams_jc_handoverreason;
+
+1¡¢¸üÐÂgams_jc_handoverreason
+2¡¢
+
+SELECT * FROM GAMS_BIZ_CONFIG gbc ;
+
+SELECT ZIFFS FROM GAMS_REIMBURSE gr WHERE gr.BILL_CODE = 'DJJZ202000009895';
