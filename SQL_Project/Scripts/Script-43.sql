d42e67ff09189af63f6d8232bb57ee0dcd102d3e
V9S <2993953259@qq.com>
2021-03-10 21:01:36 +0800

    sql

--- SQL_Project/Scripts/Script-43.sql
+++ SQL_Project/Scripts/Script-43.sql
@@ -1,41 +1,114 @@
 SELECT
-	t.id
+	t.id,
+CASE
+		to_char(t.billdefine) WHEN 'com.jiuqi.np.gams2.business.bill.InspectionEntryBillDefine' THEN '验收上账'
+		WHEN 'com.jiuqi.np.gams2.business.bill.TransferBillDefine' THEN '部门间调拨'
+		WHEN 'com.jiuqi.np.gams2.business.bill.InnerTransferBillDefine' THEN '部门内调拨'
+		ELSE '验收上账'
+	END AS lingyly,
+	t.billdefine AS biztype,
+	t.billid,
+	c.version,
+	c.creator,
+	c.createtime,
+	c.auditstate,
+	hangyfl.carddefinename AS billdefine,
+	c.objectid,
+	c.orgunit,
+	c.cardstate,
+	c.yewxlh,
+	c.billcode,
+	c.zicmc,
+	zicfl.title AS zicfl,
+	zicfl.id AS zicflid,
+	hangyfl.id AS hangyflid,
+	hangyfl.title AS hangyfl,
+	jiaoysyfx.title AS jiaoysyfx,
+	c.danj,
+	c.shul,
+	c.mianj,
+	jiazlx.title AS jiazlx,
+	c.jiaz AS jiaz,
+	c.caizxzj,
+	c.feiczxzj,
+	qudfs.title AS qudfs,
+	c.qudrq,
+	c.jizrq,
+CASE
+		WHEN ruzxs.title IS NULL THEN to_char('未入账')
+		ELSE to_char(ruzxs.title)
+	END AS ruzxs,
+	c.caiwrzrq,
+	c.yujsmzl,
+	c.kuaijpzh,
+	shiybm.name AS shiybm,
+	shiyr.name AS shiyr,
+	cunfdd.name AS cunfdd,
+	zhejzt.title AS zhejzt,
+	c.beiz,
+	c.id AS cardid,
+	c.cunfdd AS cunfddid,
+	td.cunfdd_new AS cunfddnew,
+	c.shiyr AS shiyrid
 FROM
-	GAMS_ACCEPTANCEUSE t
-LEFT JOIN gams_card c ON
+	GamsAcceptanceUseInfo AS t
+LEFT JOIN gams_card AS c ON
 	t.cardid = c.id
-LEFT JOIN GAMS_CARD_LOCK loc ON
+LEFT JOIN CardLock AS loc ON
 	loc.cardobjectid = c.objectid
-LEFT JOIN gams_jc_assetclass zicfl ON
+LEFT JOIN gams_jc_assetclass AS zicfl ON
 	zicfl.id = c.zicfl
-LEFT JOIN gams_jc_assetclass_ind hangyfl ON
+LEFT JOIN gams_jc_assetclass_ind AS hangyfl ON
 	hangyfl.id = c.hangyfl
-LEFT JOIN biz_jy00_gams_jc_jiaoyusage jiaoysyfx ON
+LEFT JOIN biz_jy00_gams_jc_jiaoyusage AS jiaoysyfx ON
 	jiaoysyfx.id = c.jiaoysyfx
-LEFT JOIN gams_jc_valuetype jiazlx ON
+LEFT JOIN gams_jc_valuetype AS jiazlx ON
 	jiazlx.id = c.jiazlx
-LEFT JOIN gams_jc_gainmanner qudfs ON
+LEFT JOIN gams_jc_gainmanner AS qudfs ON
 	qudfs.id = c.qudfs
-LEFT JOIN gams_jc_entryform ruzxs ON
+LEFT JOIN gams_jc_entryform AS ruzxs ON
 	ruzxs.id = c.ruzxs
-LEFT JOIN gams_jc_department shiybm ON
+LEFT JOIN gams_jc_department AS shiybm ON
 	shiybm.id = c.shiybm
-LEFT JOIN gams_jc_personnel shiyr ON
+LEFT JOIN gams_jc_personnel AS shiyr ON
 	shiyr.id = t.lingyr
-LEFT JOIN authz_user_identity ui ON
-	ui.USER_ID = shiyr.USERID
+LEFT JOIN gams_jc_depositary AS cunfdd ON
+	cunfdd.id = c.cunfdd
+LEFT JOIN gams_jc_deprecstate AS zhejzt ON
+	zhejzt.id = c.zhejzt
+LEFT JOIN user_identity ui ON
+	ui.userId = shiyr.userid
 LEFT JOIN gams_transfer_detail td ON
 	t.cardid = td.cardid
-LEFT JOIN AUTHZ_IDENTITY ide ON
-	ide.id = ui.IDENTITY_ID
+LEFT JOIN IDENTITY ide ON
+	ide.id = ui.identityId
 	AND (ide.name LIKE '%JQRJ_SRJYR%'
 	OR ide.name LIKE '%SHIYR%')
 WHERE
-	t.lingyzt = 0
+	c.billcode LIKE :? billcode
+	AND c.zicmc LIKE :? zicmc
+	AND c.jiaoysyfx IN (:? jiaoysyfx)
+	AND c.jiaz >= :? jiaz_min
+	AND c.jiaz <= :? jiaz_max
+	AND c.shul >=:? shul_min
+	AND c.shul <=:? shul_max
+	AND c.billcode >= :? billcode_min
+	AND c.billcode <= :? billcode_max
+	AND truncday(c.qudrq) >= truncday(:? qudrq_min)
+	AND truncday(c.qudrq) <= truncday(:? qudrq_max)
+	AND truncday(c.caiwrzrq) >= truncday(:? caiwrzrq_min)
+	AND truncday(c.caiwrzrq) <= truncday(:? caiwrzrq_max)
+	AND COALESCE(to_char(ruzxs.name), to_char('02')) IN (:? ruzxs)
+	AND truncday(c.jizrq) >= truncday(:? jizrq_min)
+	AND truncday(c.jizrq) <= truncday(:? jizrq_max)
+	AND c.shiybm IN (:? shiybm)
+	AND c.shiyr IN (:? shiyr)
+	AND c.cunfdd IN (:? cunfdd)
+	AND c.cardstate LIKE '0%'
+	AND c.shixrq>getdate()
+	AND shiyr.userid = CURRENTuserid()
+	AND t.lingyzt = 0
 	AND t.cardid IS NOT NULL
-	AND ide.id = '439A932D446C4D2AB213F7234AFE016A'
+	AND ide.id = CustomFunc('currentIdentityId')
 ORDER BY
-	c.billcode DESC
-
-SELECT * FROM NP_USER nu WHERE nu.NAME = '2015010097';
-SELECT * FROM AUTHZ_IDENTITY ai WHERE ai.NAME LIKE '%2015010097%'
+	c.billcode DESC "
