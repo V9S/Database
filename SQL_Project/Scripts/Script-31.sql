1489a877e07570cd0b26e0e433d52c407dc7840c
V9S <2993953259@qq.com>
2021-03-22 18:10:02 +0800

    sql

--- SQL_Project/Scripts/Script-31.sql
+++ SQL_Project/Scripts/Script-31.sql
@@ -1,1 +1,117 @@
-SELECT  DISTINCT RECVER  FROM GAMS_ASSETDEPREC_H RECVER
+SELECT
+	t.id,
+CASE
+		to_char(t.billdefine) WHEN 'com.jiuqi.np.gams2.business.bill.InspectionEntryBillDefine' THEN '验收上账'
+		WHEN 'com.jiuqi.np.gams2.business.bill.TransferBillDefine' THEN '部门间调拨'
+		WHEN 'com.jiuqi.np.gams2.business.bill.InnerTransferBillDefine' THEN '部门内调拨'
+		ELSE '验收上账'
+	END AS lingyly,
+	t.billdefine AS biztype,
+	t.billid,
+	c.version,
+	c.creator,
+	c.createtime,
+	c.auditstate,
+	hangyfl.carddefinename AS billdefine,
+	c.objectid,
+	c.orgunit,
+	c.cardstate,
+	c.yewxlh,
+	c.billcode,
+	c.zicmc,
+	zicfl.title AS zicfl,
+	zicfl.id AS zicflid,
+	hangyfl.id AS hangyflid,
+	hangyfl.title AS hangyfl,
+	jiaoysyfx.title AS jiaoysyfx,
+	c.danj,
+	c.shul,
+	c.mianj,
+	jiazlx.title AS jiazlx,
+	c.jiaz AS jiaz,
+	c.caizxzj,
+	c.feiczxzj,
+	qudfs.title AS qudfs,
+	c.qudrq,
+	c.jizrq,
+CASE
+		WHEN ruzxs.title IS NULL THEN to_char('未入账')
+		ELSE to_char(ruzxs.title)
+	END AS ruzxs,
+	c.caiwrzrq,
+	c.yujsmzl,
+	c.kuaijpzh,
+	shiybm.name AS shiybm,
+	shiyr.name AS shiyr,
+	cunfdd.name AS cunfdd,
+	zhejzt.title AS zhejzt,
+	c.beiz,
+	c.id AS cardid,
+	c.cunfdd AS cunfddid,
+	c.shiyr AS shiyrid
+FROM
+	GamsAcceptanceUseInfo AS t
+LEFT JOIN gams_card AS c ON
+	t.cardid = c.id
+LEFT JOIN CardLock AS loc ON
+	loc.cardobjectid = c.objectid
+LEFT JOIN gams_jc_assetclass AS zicfl ON
+	zicfl.id = c.zicfl
+LEFT JOIN gams_jc_assetclass_ind AS hangyfl ON
+	hangyfl.id = c.hangyfl
+LEFT JOIN biz_jy00_gams_jc_jiaoyusage AS jiaoysyfx ON
+	jiaoysyfx.id = c.jiaoysyfx
+LEFT JOIN gams_jc_valuetype AS jiazlx ON
+	jiazlx.id = c.jiazlx
+LEFT JOIN gams_jc_gainmanner AS qudfs ON
+	qudfs.id = c.qudfs
+LEFT JOIN gams_jc_entryform AS ruzxs ON
+	ruzxs.id = c.ruzxs
+LEFT JOIN gams_jc_department AS shiybm ON
+	shiybm.id = c.shiybm
+LEFT JOIN gams_jc_personnel AS shiyr ON
+	shiyr.id = t.lingyr
+LEFT JOIN gams_jc_depositary AS cunfdd ON
+	cunfdd.id = c.cunfdd
+LEFT JOIN gams_jc_deprecstate AS zhejzt ON
+	zhejzt.id = c.zhejzt
+LEFT JOIN user_identity ui ON
+	ui.userId = shiyr.userid
+LEFT JOIN IDENTITY ide ON
+	ide.id = ui.identityId
+	AND (ide.name LIKE '%JQRJ_SRJYR%'
+	OR ide.name LIKE '%SHIYR%')
+WHERE
+	c.billcode LIKE :? billcode
+	AND c.zicmc LIKE :? zicmc
+	AND c.jiaoysyfx IN (:? jiaoysyfx)
+	AND c.jiaz >= :? jiaz_min
+	AND c.jiaz <= :? jiaz_max
+	AND c.shul >=:? shul_min
+	AND c.shul <=:? shul_max
+	AND c.billcode >= :? billcode_min
+	AND c.billcode <= :? billcode_max
+	AND truncday(c.qudrq) >= truncday(:? qudrq_min)
+	AND truncday(c.qudrq) <= truncday(:? qudrq_max)
+	AND truncday(c.caiwrzrq) >= truncday(:? caiwrzrq_min)
+	AND truncday(c.caiwrzrq) <= truncday(:? caiwrzrq_max)
+	AND COALESCE(to_char(ruzxs.name), to_char('02')) IN (:? ruzxs)
+	AND truncday(c.jizrq) >= truncday(:? jizrq_min)
+	AND truncday(c.jizrq) <= truncday(:? jizrq_max)
+	AND c.shiybm IN (:? shiybm)
+	AND c.shiyr IN (:? shiyr)
+	AND c.cunfdd IN (:? cunfdd)
+	AND c.cardstate LIKE '0%'
+	AND c.shixrq>getdate()
+	AND shiyr.userid = CURRENTuserid()
+	AND t.lingyzt = 0
+	AND t.cardid IS NOT NULL
+	AND ide.id = CustomFunc('currentIdentityId')
+ORDER BY
+	c.billcode DESC "
+	
+	SELECT * FROM AUTHZ_USER_IDENTITY ai w;
+	B42F14F092B942079680C5B1DBBFDC50
+SELECT * FROM AUTHZ_IDENTITY ai WHERE ai.name LIKE '%2016010053%'
+
+SELECT * FROM GAMS_JC_FUNDNUMBER gjf 
--- SQL_Project/Scripts/Script-41.sql
+++ SQL_Project/Scripts/Script-41.sql
@@ -1,1 +1,82 @@
-SELECT * FROM GAMS_JC_PERSONNEL gjp ;
+SELECT
+	'' AS lingyzt,
+	COALESCE(wly.wly, 0) AS wly,
+	COALESCE(yly.yly, 0) AS yly,
+	COALESCE(bly.bly, 0) AS bly,
+	bpi.id AS taskId,
+	gt.id AS id,
+	gt.BILL_CODE AS billcode,
+	shenqr.name AS shenqr,
+	gt.shenqsj AS shenqsj,
+	diaocbm.name AS diaocbm,
+	diaorbm.name AS diaorbm
+FROM
+	GAMS2_WORKFLOW_BUS_PROCESS  bpi
+JOIN gams_transfer  gt ON
+	bpi.BUSINESS_DATA_ID = gt.id
+LEFT JOIN gams_jc_department  diaocbm ON
+	gt.diaocbm = diaocbm.id
+LEFT JOIN gams_jc_department  diaorbm ON
+	gt.diaorbm = diaorbm.id
+LEFT JOIN gams_jc_personnel  shenqr ON
+	gt.shenqr = shenqr.id
+LEFT JOIN (
+	SELECT
+		count(detail.MASTER_ID)  wly,
+		detail.MASTER_ID id
+	FROM
+		gams_transfer_detail detail
+	JOIN gams_jc_personnel pe ON
+		pe.id = detail.shiyr_new
+	WHERE
+		 (detail.isly IS NULL
+		OR detail.isly = 0)
+	GROUP BY
+		detail.MASTER_ID)  wly ON
+	gt.id = wly.id
+LEFT JOIN (
+	SELECT
+		count(detail.MASTER_ID)  yly,
+		detail.MASTER_ID id
+	FROM
+		gams_transfer_detail detail
+	JOIN gams_jc_personnel pe ON
+		pe.id = detail.shiyr_new
+	WHERE
+		 detail.isly = 1
+	GROUP BY
+		detail.MASTER_ID)  yly ON
+	yly.id = gt.id
+LEFT JOIN (
+	SELECT
+		count(detail.MASTER_ID)  bly,
+		detail.MASTER_ID id
+	FROM
+		gams_transfer_detail detail
+	JOIN gams_jc_personnel pe ON
+		pe.id = detail.shiyr_new
+	WHERE
+		detail.isly = 2
+	GROUP BY
+		detail.MASTER_ID)  bly ON
+	bly.id = gt.id
+WHERE
+	gt.lyenable = 1
+	AND EXISTS (
+	SELECT
+		1
+	FROM
+		gams_transfer_detail detail
+	JOIN gams_jc_personnel pe ON
+		pe.id = detail.shiyr_new
+	WHERE
+		(detail.isly = 0
+		OR detail.isly IS NULL)
+		AND detail.MASTER_ID = gt.id)
+	AND gt.yewlx = '部门内调拨'
+ORDER BY
+	gt.CREATE_TIME DESC;
+	
+SELECT * FROM gams_transfer_detail;
+	
+SELECT * FROM GAMS2_WORKFLOW_BUS_PROCESS;
