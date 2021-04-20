a68632027b64b7c9c60d6d3d8874eb0e08b4e7d1
V9S <2993953259@qq.com>
2021-04-20 17:59:03 +0800

    sql

--- SQL_Project/Scripts/吉林铁路建账、交接变为待提交.sql
+++ SQL_Project/Scripts/吉林铁路建账、交接变为待提交.sql
@@ -70,8 +70,6 @@ COMMIT;
 DROP TABLE delete_temp;
 
 -----交接
-
-
 delete from gams2_workflow_opinion where billid = (select id from gams_handover where bill_code = 'ZCJJ2021000002'and org_id = '456E9EA9E0000001E75604D5761203C3');
 
 create table temp as select *  from gams2_workflow_common_task where business_data_id = (select id from gams_handover where bill_code = 'ZCJJ2021000002' and org_id = '456E9EA9E0000001E75604D5761203C3');
@@ -94,7 +92,7 @@ delete from gams_card_lock where cardobjectid in (select cardid from gams_handov
 
 delete from gams_bill_annex where billid = (select id from gams_handover where bill_code = 'ZCJJ2021000002');
 
-delete from gams_card_change where cardid in (select cardid from gams_handover_detail where master_id = (select id from gams_handover where bill_code = 'ZCJJ2021000002'));
+delete from gams_card_change where cardid in (select cardid from gams_handover_detail where master_id = (select id from gams_handover where bill_code = 'ZCJJ2021000002')) AND biztype = 'com.jiuqi.np.gams2.business.bill.HandoverBillDefine';;
 
 delete from gams_card_trace  where cardid in (select cardid from gams_handover_detail where master_id = (select id from gams_handover where bill_code = 'ZCJJ2021000002')) and bizcode = 'ZCJJ2021000002';
 
@@ -106,5 +104,39 @@ UPDATE gams_handover a SET a.BILL_STATE = 0 ,  a.WORKFLOW_STATE = 0 WHERE a.BILL
 
 
 SELECT * FROM GAMS_INSPECTIONENTRY_DETAIL gid 
+
+
+-----部门内交接
+delete from gams2_workflow_opinion where billid = (select id from GAMS_TRANSFER gt  where bill_code = 'ZCJJ2021000002'and org_id = '456E9EA9E0000001E75604D5761203C3');
+
+create table temp as select *  from gams2_workflow_common_task where business_data_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002' and org_id = '456E9EA9E0000001E75604D5761203C3');
+
+delete from gams2_workflow_common_task where business_data_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002' and org_id = '456E9EA9E0000001E75604D5761203C3');
+
+delete from gams2_task_actor where task_id in (select id from temp a where a.business_data_id=(select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002'and org_id = '456E9EA9E0000001E75604D5761203C3' ) );
+
+delete from gams_task_trace where billid in (select business_data_id from temp a where a.business_data_id=(select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002'and org_id = '456E9EA9E0000001E75604D5761203C3' ) );
+
+delete from gams2_workflow_bus_process where business_data_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002' and org_id = '456E9EA9E0000001E75604D5761203C3');
+
+delete from gams2_workflow_workitem where workflow_process_ins_id in (select workflow_process_ins_id from temp a where a.business_data_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002'and org_id = '456E9EA9E0000001E75604D5761203C3' ));
+
+delete from gams2_workflow_process_ins where id in (select workflow_process_ins_id from temp a where a.business_data_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002'and org_id = '456E9EA9E0000001E75604D5761203C3' ));
+
+drop table temp;
+
+delete from gams_card_lock where cardobjectid in (select cardobjectid from GAMS_TRANSFER_detail where master_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002'));
+
+delete from gams_bill_annex where billid = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002');
+
+delete from gams_card_change where cardid in (select cardid from GAMS_TRANSFER_detail where master_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002')) AND biztype = 'com.jiuqi.np.gams2.business.bill.TransferBillDefine';
+
+delete from gams_card_trace  where cardid in (select cardid from GAMS_TRANSFER_detail where master_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002')) and bizcode = 'ZCJJ2021000002';
+
+UPDATE GAMS_TRANSFER a SET a.BILL_STATE = 0 ,  a.WORKFLOW_STATE = 0 WHERE a.BILL_CODE = 'YSSZ2021000006';
+
+--delete from GAMS_TRANSFER_detail where master_id = (select id from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002');
+--
+--delete from GAMS_TRANSFER where bill_code = 'ZCJJ2021000002';
 
  
